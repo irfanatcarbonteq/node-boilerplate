b427fb099ed1a2a650fc3d1c4ccad4566a5f1615
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-06-21 11:46:49 +0500

    Add http middleware tests

--- test/fakeObjects/user.js
+++ test/fakeObjects/user.js
@@ -2,6 +2,6 @@ const faker = require("faker");
 user = {
   name: faker.name.findName(),
   email: faker.internet.email().toLowerCase(),
-  password: faker.internet.password()
+  password: "12345678",
 };
 module.exports = user;
